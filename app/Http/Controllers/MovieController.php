8cff620ae108c320f5e7b62d633e30975fc86c11
Por <porthachapong@gmail.com>
2017-05-23 13:48:55 +0700

    laravel code

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ *
+ */
+class MovieController extends Controller
+{
+
+  function __construct()
+  {
+    # code...
+  }
+
+
+public  function index(){
+return "hello MovieController";
+}
+
+
+public function band()
+{
+$this->data = array(
+'tor' => 'Por',
+'movie' => 'black'
+
+);
+return view('movie/band', $this->data);
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+ ?>
---
+++ resources/views/movie/band.blade.php
@@ -0,0 +1,3 @@
+<h1>BladeBand </h1>
+<h2>band เท่ากับ  {{ $tor }} </h2>
+<h2>movie เท่ากับ {{ $movie }}</h2>
